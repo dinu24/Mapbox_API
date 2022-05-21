6e89f8e020309544f22b09bd3b3ab83e0538a58a
dinu24 <dinukreji24@gmail.com>
2022-05-21 21:00:21 +0530

    added current location finder

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGludTI0IiwiYSI6ImNsM2Z4NXNicTA5a3AzZHQ1cnBqajdrNncifQ.OoncHGFxe7E4otOMC-aQ_w';
-const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v11',
-center: [77.5, 9.5],
-zoom: 7
-});
 
-map.addControl(
-    new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl
-    })
-    );
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(position => {
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [position.coords.longitude, position.coords.latitude],
+            zoom: 14
+        });
+    });
+} else {
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [77.5, 9.5],
+        zoom: 7
+    });
+}
+
